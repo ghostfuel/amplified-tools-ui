357e3d57839391d82d206b90c61abf023f5c487f
ghostfuel <10226711+ghostfuel@users.noreply.github.com>
2022-07-24 16:10:22 +0100

    Added further props for edge dropping to WorkflowRenderer

--- src/components/WorkflowRenderer/WorkflowRenderer.tsx
+++ src/components/WorkflowRenderer/WorkflowRenderer.tsx
@@ -1,5 +1,5 @@
 import { CSSProperties, FunctionComponent, useMemo } from 'react';
-import ReactFlow, { Controls, Node, Edge, OnNodesChange, OnEdgesChange, OnConnect, Background, DefaultEdgeOptions } from 'react-flow-renderer';
+import ReactFlow, { Controls, Node, Edge, OnNodesChange, OnEdgesChange, OnConnect, Background, DefaultEdgeOptions, Connection } from 'react-flow-renderer';
 import ActionNode from './Nodes/ActionNode';
 import FilterNode from './Nodes/FilterNode';
 import SelectorNode from './Nodes/SelectorNode';
@@ -15,10 +15,13 @@ type WorkflowRendererProps = {
     onNodesChange: OnNodesChange,
     onEdgesChange: OnEdgesChange,
     onConnect: OnConnect,
+    onEdgeUpdate: (oldEdge: Edge, newConnection: Connection) => void
+    onEdgeUpdateStart: () => void
+    onEdgeUpdateEnd: (update: any, edge: Edge) => void
 }
 
 const WorkflowRenderer: FunctionComponent<WorkflowRendererProps> = (props: WorkflowRendererProps) => {
-    const { nodes, edges, onNodesChange, onEdgesChange, onConnect, defaultEdgeOptions } = props;
+    const { nodes, edges, onNodesChange, onEdgesChange, onConnect, defaultEdgeOptions, onEdgeUpdate, onEdgeUpdateEnd, onEdgeUpdateStart } = props;
     const nodeTypes = useMemo(() => ({ source: SourceNode, action: ActionNode, filter: FilterNode, sorter: SorterNode, selector: SelectorNode }), []);
 
     return (
@@ -31,6 +34,9 @@ const WorkflowRenderer: FunctionComponent<WorkflowRendererProps> = (props: Workf
             onNodesChange={onNodesChange}
             onEdgesChange={onEdgesChange}
             onConnect={onConnect}
+            onEdgeUpdate={onEdgeUpdate}
+            onEdgeUpdateStart={onEdgeUpdateStart}
+            onEdgeUpdateEnd={onEdgeUpdateEnd}
             maxZoom={1.3}
             fitView
         >
